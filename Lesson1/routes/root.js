a422dae0b64692d21eef8559770bf6716fd6e52d
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-05-03 21:11:01 -0500

    typos corrected

--- Lesson1/routes/root.js
+++ Lesson1/routes/root.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('^/$ | /index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/views', '/index.html'))
 }) //getting index.html file if it matches any of the three /index routes
 
 module.exports = router;
--- Lesson2/routes/root.js
+++ Lesson2/routes/root.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('^/$ | /index(.html)?', (req, res) => {
+router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 }) //getting index.html file if it matches any of the three /index routes
 
