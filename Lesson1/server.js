56b9b969dbb01cb176a7f1341b330e8c98d17d19
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-05-03 20:25:44 -0500

    404 erro page and handling added to server

--- Lesson1/server.js
+++ Lesson1/server.js
@@ -8,4 +8,15 @@ app.use('/', express.static(path.join(__dirname, '/public')));// telling express
 
 app.use('/', require('./routes/root'));
 
+app.all('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')) { // if request is html
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) { // if request is json
+        res.json({ message: "404 Not Found" })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT} LETS GO!`));
