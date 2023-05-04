dd9b280dc2e3cddc450fb8fe77d6f14e6ee24dbb
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-05-03 21:16:44 -0500

    errorHandler middleware -- logs error in error log errorHandler mware logs errors

--- Lesson2/server.js
+++ Lesson2/server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const path = require('path');
 const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
@@ -14,15 +15,17 @@ app.use(express.static('public')); // telling express where to find static files
 
 app.use('/', require('./routes/root'));
 
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if(req.accepts('html')) { // if request is html
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if (req.accepts('json')) { // if request is json
-//         res.json({ message: '404 Not Found' })
-//     } else {
-//         res.type('txt').send('404 Not Found')
-//     }
-// })
+app.all('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')) { // if request is html
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) { // if request is json
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT} LETS GO!`));
