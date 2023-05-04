c1177a7887715ef4acc632cc1ea7632bced3675c
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-05-04 09:36:06 -0500

    Lesson3 started - mongoDB

---
+++ Lesson3/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000', // strings that say what origins are allowed -- 3000 is where react app is
+    //fake urls
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
