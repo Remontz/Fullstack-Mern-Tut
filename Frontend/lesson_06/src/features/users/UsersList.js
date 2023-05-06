c71ff50f5f5e0500541a3bf0166d777872446d51
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-05-06 16:25:04 -0500

    redux implemented with usersApiSlice and notesApiSlice

--- Frontend/lesson_06/src/features/users/UsersList.js
+++ Frontend/lesson_06/src/features/users/UsersList.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useGetUsersQuery } from "./usersApiSlice";
+import User from "./User";
 
 const UsersList = () => {
-  return (
-    <h1>UsersList</h1>
-  )
-}
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery();
 
-export default UsersList
+  let content;
+
+  if (isLoading) content = <p>Loading...</p>;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = users;
+
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
+
+    content = (
+      <table className="table table--users">
+        <thead className="table_thead">
+          <tr>
+            <th scope="col" className="table_th user_username">
+              Username
+            </th>
+            <th scope="col" className="table_th user_username">
+              Roles
+            </th>
+            <th scope="col" className="table_th user_username">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+  return content;
+};
+
+export default UsersList;
